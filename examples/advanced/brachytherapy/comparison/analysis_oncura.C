bb38776a46806eda40eee8e2b6121b1f34c28ca7
SR <1067651217@qq.com>
2020-11-19 11:17:11 +0800

    delete

--- examples/advanced/brachytherapy/comparison/analysis_oncura.C
+++
@@ -1,9 +0,0 @@
-{
-gROOT->ProcessLine(".L TG43_relative_dose.C");
-Read("Oncura", "livermore");
-Read("Oncura", "penelope");
-Read("Oncura", "opt0");
-Read("Oncura", "opt4");
-gROOT->ProcessLine(".x compare_6711.C");
-}
-
